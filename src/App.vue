897037961fa098242ae278a526496a15199731a0
Vittorio <vittoriopaiva@gmail.com>
2024-10-16 11:33:11 -0300

    teste

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+export default {
+  components: { Banner, ConteudoPrincipal}
+}
+
+</script>
+
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+
+  <h1 class="titulo">
+    Texto do App.vue
+  </h1>
+</template>
+
+<style scoped>
+.titulo {
+  color: red;
+}
+</style>
